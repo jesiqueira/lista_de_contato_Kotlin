7a8ab453b5094ae94f72cfc49f82b4836f8433d0
José Edmar de Siqueira <edmar.ade@gmail.com>
2021-03-13 14:40:56 -0300

    Criado Menu de opções

--- app/src/main/java/com/edmar/listadecontato/MainActivity.kt
+++ app/src/main/java/com/edmar/listadecontato/MainActivity.kt
@@ -2,6 +2,10 @@ package com.edmar.listadecontato
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -41,5 +45,32 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+        //Criar Toast para teste de menu selecionado
+    private fun showToast(messagem: String){
+        Toast.makeText(this, messagem, Toast.LENGTH_SHORT).show()
+    }
+
+        //Criar menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // inflar o menu criado
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    //Verificar qual menu está selecionado se não tiver menu selecionar execute o default
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de Menu 1")
+                return  true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de menu 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
